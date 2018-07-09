96cbf21fa3d3fd750e476e305634fb39d8e2f1d8
DESKTOP-4V8BJL8\SY <songyangmri@gmail.com>
2018-07-09 10:53:26 +0800

    Add statistic on the detection.

--- MeDIT/Statistics.py
+++ MeDIT/Statistics.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.stats import sem
 from sklearn.metrics import roc_auc_score
-
+from scipy import ndimage
+from MeDIT.ImageProcess import RemoveSmallRegion
 
 def AUC_Confidence_Interval(y_true, y_pred, CI_index=0.95):
     AUC = roc_auc_score(y_true, y_pred)
@@ -35,3 +36,50 @@ def AUC_Confidence_Interval(y_true, y_pred, CI_index=0.95):
 
     print('AUC is {:.3f}, Confidence interval : [{:0.3f} - {:0.3}]'.format(AUC, confidence_lower, confidence_upper))
     return AUC, CI, sorted_scores
+
+def StatisticDetection(prediction_map, label_map, threshold_value_on_overlap=0.5):
+    '''
+    To Calculate the staitstic value of the detection results.
+    For each region of the label map, if the prediction overlaps above threshold value, true positive value + 1.
+    For each region of the label map, if the prediction overlaps under threshold value, false negative value + 1.
+    For each region of the prediction map, if the label overlaps under threshold value, false positive value + 1.
+
+    :param prediction_map: the detection result (binary image)
+    :param label_map: the ground truth (binary image)
+    :param threshold_value_on_overlap: the threshold value to estiamte the reuslt.
+    :return: the true positive value, false positive value, and the false negative value.
+    '''
+    image_shape = prediction_map.shape
+
+    prediction_im, prediction_nb = ndimage.label(prediction_map)
+    label_im, label_nb = ndimage.label(label_map)
+
+    true_positive = 0
+    false_negative = 0
+    false_positive = 0
+
+    for index in range(1, label_nb+1):
+        x_label, y_label = np.where(label_im == index)
+        index_label = XY2Index([x_label, y_label], image_shape)
+
+        x_pred, y_pred = np.where(prediction_im > 0)
+        index_pred = XY2Index([x_pred, y_pred], image_shape)
+        inter_index = np.intersect1d(index_label, index_pred)
+
+        if inter_index.size / index_label.size >= threshold_value_on_overlap:
+            true_positive += 1
+        else:
+            false_negative += 1
+
+    for index in range(1, prediction_nb+1):
+        x_pred, y_pred = np.where(prediction_im == index)
+        index_pred = XY2Index([x_pred, y_pred], image_shape)
+
+        x_label, y_label = np.where(label_im > 0)
+        index_label = XY2Index([x_label, y_label], image_shape)
+        inter_index = np.intersect1d(index_label, index_pred)
+
+        if inter_index.size / index_pred.size < threshold_value_on_overlap:
+            false_positive += 1
+
+    return true_positive, false_positive, false_negative
