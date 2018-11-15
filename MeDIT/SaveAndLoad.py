51a8c0542c1299013ac3853d60fe533e76a20fb3
DESKTOP-4V8BJL8\SY <songyangmri@gmail.com>
2018-11-15 11:40:28 +0800

    Add load Tob (SPIN) file Add ProstateSegment2.5D class

--- MeDIT/SaveAndLoad.py
+++ MeDIT/SaveAndLoad.py
@@ -503,5 +503,32 @@ def read_roi_zip(zip_path):
         rois.update(read_roi_file(zf.open(n)))
     return rois
 
+def GenerateROIFromSPIN(tob_file_path, ref_image):
+    '''
+    This function was to read .tob file, which was designed by SPIN, MRInnovation, (by Dr. Haacke, Ying Wang.)
+    :param tob_file_path: The file with '.tob'
+    :param ref_image: The reference image, which provided the shape of the image
+    :return: The generated ROI. Different cores was assigned to different values. The first 2 dimensions were swapped for visualization.
+     By Jie Wu, Nov-11-18
+    '''
+    if isinstance(ref_image, str):
+        ref_image = sitk.ReadImage(ref_image)
 
+    image_shape = ref_image.GetSize()[0:3]
+    recon_data = np.zeros(image_shape)
 
+    index_list = np.fromfile(tob_file_path, dtype=np.uint32)
+
+    total_roi = index_list[1]
+    roi_number_index = 2
+    for roi_index in range(total_roi):
+        roi_point_number = index_list[roi_number_index] // 4
+        print('The number of array: ', roi_point_number)
+        for roi_point_index in range(1, roi_point_number):
+            recon_data[index_list[roi_number_index + roi_point_index * 4 + 1],
+                       index_list[roi_number_index + roi_point_index * 4 + 2],
+                       index_list[roi_number_index + roi_point_index * 4 + 3]] = roi_index + 1
+        roi_number_index += roi_point_number * 4 + 1
+
+    recon_data = np.swapaxes(recon_data, 0, 1)
+    return recon_data
